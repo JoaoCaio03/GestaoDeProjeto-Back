7a08123f3c33aae8afb61d919c2deb5f5c056d79
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2026-03-11 00:28:54 -0300

    refactor: use fastify for better ram usage

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaExceptionFilter } from './common/filters/prisma-exception.filter';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
   const port = process.env.PORT || 3000;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   app.useGlobalFilters(new PrismaExceptionFilter());
 
   app.enableCors();
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
