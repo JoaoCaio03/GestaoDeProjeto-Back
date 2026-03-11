59b87c3e9525f21f13ffe281657deee0c3dade85
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2026-03-11 00:04:14 -0300

    feat: update to use port from env

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,11 @@ import { AppModule } from './app.module';
 import { PrismaExceptionFilter } from './common/filters/prisma-exception.filter';
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new PrismaExceptionFilter());
 
-  app.enableCors()
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(port);
 }
 bootstrap();
